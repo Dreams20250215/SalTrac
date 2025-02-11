ba8735c5aef10d5b2db0df1f875ea4f0c59d64aa
Yoshiki-fu <yf140817@gmail.com>
2025-02-11 20:16:02 +0900

    add flaskr

---
+++ server/flaskr/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+
+
+import os 
+from flask import Flask 
+
+# Application factory
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)     # instance_relative_configは設定ファイルの場所がインスタンスフォルダから相対的に示されることをappに伝える
+    app.config.from_mapping(
+                        SECRET_KEY='dev',       # データを安全に保つため
+                        DATAVASE = os.path.join(app.instance_path, 'flaskr.sqlite'),        # SQLiteデータベースファイルのパス
+                        )
+    
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)        # 標準設定の上書き。デプロイ時の時には、本当のSECRET_KEYを設定するために使用
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    return app
--- server/main.py
+++ server/main.py
@@ -1,3 +1,27 @@
+from flask import Flask, request, redirect, url_for
+
+app = Flask(__name__)
+
+# routeデコレータで、どのURLが引き金になるべきかをFlaskに伝える
+# URL "/"（ブラウザ）の時、home関数が呼ばれる
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+# ログイン画面の処理
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['passward']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/passward'
+
+
 
 if __name__ == "__main__":
-    print("Hello")
+    app.run(host="0.0.0.0", port=5000, debug=True)      # host="0.0.0.0"で全ての公開されているIP上でアクセスを受け付け
+    app.logger.debug('A value for debugging')
+    app.logger.warning('A warning occurred (%d apples)', 42)
+    app.logger.error('An error occurred')
