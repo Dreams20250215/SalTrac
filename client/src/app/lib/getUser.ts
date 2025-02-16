71fe5365d5f9ebbb2be3a109cab091f928a65622
bq22005 <bq22005@shibaura-it.ac.jp>
2025-02-16 12:24:15 +0900

    Update signup function

--- client/src/app/lib/getUser.ts
+++ client/src/app/lib/getUser.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { redirect } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 const API_URL = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
